b96b8a7209e5340c55462d05a7862e78fc4a11fb
Mikhail <60521305+migechal@users.noreply.github.com>
2020-04-25 14:39:49 -0700

    re-did server.sh

---
+++ server.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt install 
+sudo apt-get install git openjdk-8-jre-headless
+sudo java -version
+mkdir Spigot_Server && cd Spigot_Server
+wget -O BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
+git config --global --unset core.autocrlf
+java -jar BuildTools.jar --rev 1.15.2
+java -Xms1G -Xmx1G -jar spigot.jar > start.sh
+chmod +x start.sh
+sudo ./start.sh
