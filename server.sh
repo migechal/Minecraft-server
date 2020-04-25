5cd96d8b84f63d9e0f58a7ed191871a38cca9164
Mikhail <60521305+migechal@users.noreply.github.com>
2020-04-25 14:42:07 -0700

    Update server.sh

--- server.sh
+++ server.sh
@@ -7,6 +7,6 @@ mkdir Spigot_Server && cd Spigot_Server
 wget -O BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
 git config --global --unset core.autocrlf
 java -jar BuildTools.jar --rev 1.15.2
-java -Xms1G -Xmx1G -jar spigot.jar > start.sh
+echo java -Xms1G -Xm4G -jar spigot.jar > start.sh
 chmod +x start.sh
 sudo ./start.sh
